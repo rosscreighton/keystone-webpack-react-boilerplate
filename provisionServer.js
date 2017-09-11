01887236179a4ea40356acd7f987e86cd86f7a93
ross creighton <rosson.creighton@gmail.com>
2017-09-11 01:03:05 -0400

    working on server provisioning script

--- provisionServer.js
+++ provisionServer.js
@@ -12,21 +12,19 @@ const DOClient = new doapi({
 });
 
 async function main() {
+  //const droplets = await DOClient.dropletGetAll();
+  //droplets.forEach(droplet => {
+    //DOClient.dropletDestroy(droplet.id);
+  //})
   const dropletId = await createDroplet();
   const dropletAddress = await getDropletAddress(dropletId);
-  const ssh = new nodeSSH();
-
-  await ssh.connect({
-    host: dropletAddress,
-    username: 'root',
-    privateKey: '/Users/rosscreighton/.ssh/id_rsa'
-  });
-
-  await installNode(ssh);
+  const sshClient = new nodeSSH();
+  await connectSSH(sshClient, dropletAddress);
+  await installNginx(sshClient);
 }
 
 async function createDroplet() {
-  console.log(`creating droplet: ${dropletName}`);
+  console.log(`creating droplet with name: ${dropletName}`);
 
   const res = await DOClient.dropletNew({
     name: dropletName,
@@ -40,13 +38,41 @@ async function createDroplet() {
 }
 
 async function getDropletAddress(id) {
-  console.log(`getting droplet address: ${dropletName}`);
-  const res = await DOClient.dropletGet(id);
-  return res.networks.v4[0].ip_address;
+  console.log(`getting droplet address`);
+  const info = await DOClient.dropletGet(id);
+  const networkInfo = info.networks.v4[0];
+
+  if (networkInfo) {
+    return networkInfo.ip_address;
+  } else {
+    return getDropletAddress(id);
+  }
 }
 
-async function installNode(ssh) {
+async function connectSSH(sshClient, dropletAddress) {
+  console.log(`connecting to droplet @ ${dropletAddress}`);
+  try {
+    const res = await sshClient.connect({
+      host: dropletAddress,
+      username: 'root',
+      privateKey: '/Users/rosscreighton/.ssh/id_rsa',
+    });
+    console.log('connection successful');
+    return res;
+  } catch (e) {
+    console.log(e);
+    console.log('could not connect. trying again in 5000ms');
+    await new Promise(resolve => setTimeout(() => { resolve('waited') }, 5000));
+    return connectSSH(sshClient, dropletAddress);
+  }
+}
 
+async function installNginx(sshClient) {
+  console.log(`installing nginx`);
+  const update = await sshClient.execCommand('apt-get update');
+  console.log(update.stderr);
+  const install = await sshClient.execCommand('apt-get -y install nginx');
+  console.log(install.stderr);
 }
 
 main();
