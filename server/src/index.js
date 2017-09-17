3e372a59ea2b02176c7eb8ef2159577ac773a23c
ross creighton <rosson.creighton@gmail.com>
2017-09-17 17:01:30 -0400

    adds keystone env to environment config

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import keystone from 'keystone';
 import routes from './routes';
 
 keystone.init({
+  'env': process.env.NODE_ENV,
   'name': 'MISTER',
   'cookie secret': 'secure string',
   'user model': 'User',
