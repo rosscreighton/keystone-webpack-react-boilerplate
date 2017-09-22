ad5fff20896edd20b7952b64d6fd5e289a4235ab
ross creighton <rosson.creighton@gmail.com>
2017-09-22 18:10:39 -0400

    use node env plugin to optimize production builds

--- client/webpack-production.config.js
+++ client/webpack-production.config.js
@@ -1,5 +1,14 @@
+/* eslint quote-props: "off" */
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const shared = require('./webpack-shared.config.js');
 
 module.exports = merge(shared, {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
 });
