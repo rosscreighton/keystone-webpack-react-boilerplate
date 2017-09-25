2e812a95d2d6a057d5a522bb855c3ff2e4044ef8
ross creighton <rosson.creighton@gmail.com>
2017-09-24 21:53:51 -0400

    preact-router is much lighter weight

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Router } from 'preact-router';
 import LandingPage from '../LandingPage';
 import CaseStudiesPage from '../CaseStudiesPage';
 import CompanyPage from '../CompanyPage';
@@ -8,21 +8,21 @@ import CareersPage from '../CareersPage';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li><Link to="/">home</Link></li>
-          <li><Link to="/case-studies">case studies</Link></li>
-          <li><Link to="/company">company</Link></li>
-          <li><Link to="/contact">contact</Link></li>
-          <li><Link to="/careers">careers</Link></li>
-        </ul>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/case-studies" component={CaseStudiesPage} />
-        <Route path="/company" component={CompanyPage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/careers" component={CareersPage} />
-      </div>
-    </Router>
+    <div>
+      <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/case-studies">case studies</a></li>
+        <li><a href="/company">company</a></li>
+        <li><a href="/contact">contact</a></li>
+        <li><a href="/careers">careers</a></li>
+      </ul>
+      <Router>
+        <LandingPage path="/" />
+        <CaseStudiesPage path="/case-studies" />
+        <CompanyPage path="/company" />
+        <ContactPage path="/contact" />
+        <CareersPage path="/careers" />
+      </Router>
+    </div>
   );
 }
--- server/src/routes/views/index.js
+++ server/src/routes/views/index.js
@@ -2,6 +2,7 @@ import keystone, { View } from 'keystone';
 
 export default function (req, res) {
   const view = new View(req, res);
+
   view.render('index', {
     clientBundleFile: keystone.get('client bundle file'),
   });
